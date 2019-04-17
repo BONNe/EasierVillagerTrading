bc317ce993c610f660115ab3f7ea2bb773d5cf28
BONNe <bonne@bonne.id.lv>
2019-04-17 09:15:15 +0300

    Update config file a bit.

--- src/main/java/lv/id/bonne/easiervillagertrading/config/Config.java
+++ src/main/java/lv/id/bonne/easiervillagertrading/config/Config.java
@@ -10,55 +10,55 @@ public class Config
 	static void init()
 	{
 		ConfigurationLoader.CLIENT_BUILDER.
-				comment("Easier Villager Trading configuration").
+				comment(" Easier Villager Trading configuration").
 				push("EasierVillagerTrading");
 
 		sellAll = ConfigurationLoader.CLIENT_BUILDER.
-			comment("Should sell all be enabled on GUI opening.").
+			comment(" Should sell all be enabled on GUI opening.").
 			define("default_sell_all", false);
 
 		buttonType = ConfigurationLoader.CLIENT_BUILDER.
-			comment("There are 3 types of button: " +
-                "\tOriginal button, that was implemented in original mod. " +
-                "\tOriginal button without enchant text. " +
-                "\tCompact button with Minecraft button design.").
+			comment(" There are 3 types of button: " +
+                "\n\t ORIGINAL_BUTTON, that was implemented in original mod. " +
+                "\n\t ORIGINAL_BUTTON_NO_ENCHANT, original button without enchant text. " +
+                "\n\t COMPACT_BUTTON with Minecraft button design.").
 			defineEnum("button_type", ButtonType.ORIGINAL_BUTTON, ButtonType.values());
 
 		panelSide = ConfigurationLoader.CLIENT_BUILDER.
-			comment("All recipe buttons can be positioned in 3 sides: " +
-                "Left side, Right side and Top side.").
+			comment(" All recipe buttons can be positioned in 3 sides: " +
+                "\n\t LEFT_SIDE, RIGHT_SIDE and TOP_SIDE.").
 			defineEnum("panel_side", SideType.LEFT_SIDE, SideType.values());
 
 		alignWithGUI = ConfigurationLoader.CLIENT_BUILDER.
-			comment("Should buttons be aligned with MerchantGUI.").
+			comment(" Should buttons be aligned with MerchantGUI.").
 			define("align_with_gui", false);
 
 		offsetFromGUI = ConfigurationLoader.CLIENT_BUILDER.
-			comment("How many pixels from all sides of the GUI the trades list will be shown.").
+			comment(" How many pixels from all sides of the GUI the trades list will be shown.").
 			defineInRange("offset_from_gui", 5, 0, Integer.MAX_VALUE);
 
 		offsetFromTopSide = ConfigurationLoader.CLIENT_BUILDER.
-			comment("How many pixels from Top side of the Window the trades list will be shown. " + "\n" +
-				"If aligning with GUI is enabled, then this option is ignored for Left and Right button position.").
+			comment(" How many pixels from Top side of the Window the trades list will be shown. " + "\n" +
+				" If aligning with GUI is enabled, then this option is ignored for Left and Right button position.").
 			defineInRange("offset_from_top", 5, 0, Integer.MAX_VALUE);
 
 		offsetFromBottomSide = ConfigurationLoader.CLIENT_BUILDER.
-			comment("How many pixels from Bottom side of the Window the trades list will be shown. " + "\n" +
-				"If aligning with GUI is enabled, then this option is ignored for Left and Right button position.").
+			comment(" How many pixels from Bottom side of the Window the trades list will be shown. " + "\n" +
+				" If aligning with GUI is enabled, then this option is ignored for Left and Right button position.").
 			defineInRange("offset_from_bottom", 5, 0, Integer.MAX_VALUE);
 
 		offsetFromRightSide = ConfigurationLoader.CLIENT_BUILDER.
-			comment("How many pixels from Right side of the Window the trades list will be shown. " + "\n" +
-				"If aligning with GUI is enabled, then this option is ignored for Top button position.").
+			comment(" How many pixels from Right side of the Window the trades list will be shown. " + "\n" +
+				" If aligning with GUI is enabled, then this option is ignored for Top button position.").
 			defineInRange("offset_from_right", 5, 0, Integer.MAX_VALUE);
 
 		offsetFromLeftSide = ConfigurationLoader.CLIENT_BUILDER.
-			comment("How many pixels from Left side of the Window the trades list will be shown. " + "\n" +
-				"If aligning with GUI is enabled, then this option is ignored for Top button position.").
+			comment(" How many pixels from Left side of the Window the trades list will be shown. " + "\n" +
+				" If aligning with GUI is enabled, then this option is ignored for Top button position.").
 			defineInRange("offset_from_left", 5, 0, Integer.MAX_VALUE);
 
 		delayBetweenActions = ConfigurationLoader.CLIENT_BUILDER.
-			comment("This allows to bypass NoCheatPlus defense. Recipes will be processed slower, but they will work.").
+			comment(" This allows to bypass NoCheatPlus defense. Recipes will be processed slower, but they will work.").
 			defineInRange("delay_between_actions", 100, 0, 10000);
 
 		ConfigurationLoader.CLIENT_BUILDER.pop();
